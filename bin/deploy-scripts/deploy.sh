b72ba2da3df8fa246861ca3ce70c9bf655e0563e
David Craddock <contact@davidcraddock.net>
2024-01-28 14:38:32 +0000

    Force clone

--- bin/deploy-scripts/deploy.sh
+++ bin/deploy-scripts/deploy.sh
@@ -5,7 +5,7 @@ set -e
 sudo mkdir -p /opt/vworkbench
 sudo chown -R root:users /opt/vworkbench
 sudo chmod -R 770 /opt/vworkbench
-git clone git@github.com:wordswords/vworkbench.git /opt/vworkbench
+git clone -f git@github.com:wordswords/vworkbench.git /opt/vworkbench
 
 cd /opt/vworkbench/bin/deploy-scripts
 ./deploy-part-0.zsh # pre requisite packages install
